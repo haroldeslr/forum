cad3410c4c7cabe8663cfcb8431c9a237d0fba0f
haroldeslr <asunakazuto1621@gmail.com>
2024-07-29 05:47:51 +0800

    Add required attributes in post and comment form

--- views/post.view.php
+++ views/post.view.php
@@ -54,7 +54,7 @@
                                     rows="3" 
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
                                     placeholder="Enter your comment here" 
-                                    ><?= $_POST['body'] ?? '' ?></textarea>
+                                    required><?= $_POST['body'] ?? '' ?></textarea>
 
                                 <?php if (isset($errors['content'])) : ?>
                                     <p class="text-red-500 text-xs mt-2"> <?= $errors['content'] ?></p>
